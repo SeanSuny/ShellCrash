e6ef1c21d1230da94c4936a3d4627931cd829d4e
Sean <20199755+SeanSuny@users.noreply.github.com>
2026-01-03 18:23:59 +0800

    Update

--- crash/crashrun.sh
+++ crash/crashrun.sh
@@ -4,12 +4,8 @@ source /etc/profile
 # 检测并补全文件
 if [ -z "$(ls -A /etc/ShellCrash)" ] || [ ! -f "/etc/ShellCrash/start.sh" ]; then
     # 如果文件夹为空，则运行指定命令
-    cp -r /etc/ShellCrash_bak /etc/ShellCrash
+    cp -r /etc/ShellCrash_bak/* /etc/ShellCrash
 fi
 
 # 启动服务
-/etc/ShellCrash/start.sh start
-echo "ShellCrash启动成功，请进入容器，输入crash进行管理！"
-
-# 保持容器运行
-exec sh
+crash -s start
