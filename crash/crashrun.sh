f5f0de201964fc585c3894f724fdb0049de24502
Sean <20199755+SeanSuny@users.noreply.github.com>
2026-01-03 17:08:53 +0800

    Update

--- crash/crashrun.sh
+++ crash/crashrun.sh
@@ -1,11 +1,2 @@
 #!bin/sh
-source /etc/profile
-
-# 检测并补全文件
-if [ -z "$(ls -A /etc/ShellCrash)" ] || [ ! -f "/etc/ShellCrash/start.sh" ]; then
-    # 如果文件夹为空，则运行指定命令
-    cp -rL /etc/ShellCrash_bak/* /etc/ShellCrash
-fi
-
-# 启动服务
-crash -s start
+exec sh
